9de2450da52c1f4b7ecbf3c1f0f37d27a484f03b
Artem Tarasov <droidapp@icloud.com>
2021-12-15 01:30:12 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+
+public class Movie {
+    private int id;
+    private String pictureURL;
+    private String name;
+    private String genre;
+
+}
